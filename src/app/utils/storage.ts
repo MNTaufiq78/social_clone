8c56745c821e2a78c281cb4406eb8be720e3ae56
MNTaufiq78 <gydmnt@gmail.com>
2025-03-07 10:44:01 +0530

    commit

--- src/app/utils/storage.ts
+++ src/app/utils/storage.ts
@@ -1,17 +1,16 @@
 import { supabase } from "@/app/lib/supabase";
 
 export const uploadAvatar = async (file: File, userId: string) => {
-    const filePath = `${userId}/${Date.now()}-${file.name}`;
-    const { data, error } = await supabase.storage
-      .from("/uploads")
-      .upload(filePath, file);
-  
-    if (error) {
-      console.error("Upload Error:", error.message);
-      return null;
-    }
-  
-    const { data: publicUrlData } = supabase.storage.from("/uploads").getPublicUrl(filePath);
-    return publicUrlData.publicUrl;
-  };
-  
+  const filePath = `${userId}/${Date.now()}-${file.name}`;
+  const { data: _, error } = await supabase.storage
+    .from("/uploads")
+    .upload(filePath, file);
+
+  if (error) {
+    console.error("Upload Error:", error.message);
+    return null;
+  }
+
+  const { data: publicUrlData } = supabase.storage.from("/uploads").getPublicUrl(filePath);
+  return publicUrlData.publicUrl;
+};
